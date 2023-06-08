fa051b068b8995ab11751de775e7bc01065dca8c
Helen Morris <morris_helen@hotmail.com>
2023-06-08 15:31:48 +0100

    initial commit

--- create-repo.sh
+++ create-repo.sh
@@ -4,6 +4,9 @@ if [[ ! -d ./personal-projects/$1 ]]; then
     mkdir ../$1
 fi
 
+git init
+git remote add origin git@github.com:$2/$1.git
+
 echo "# $1" >> ../$1/README.md
 ignore_files=('venv/' '**/*.pyc' '**/*pychache' '**/*_cache' '.env')
 
@@ -13,3 +16,7 @@ done
 
 mkdir ../$1/src ../$1/tests
 touch ../$1/src/main.py ../$1/tests/test-main.py
+
+git add .
+git commit -m "initial commit"
+git push origin main
