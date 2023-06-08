6f4e2cbc8e9c26311d44c9526470fac79ed4f04a
Helen Morris <morris_helen@hotmail.com>
2023-06-08 15:41:45 +0100

    remove git commands

--- create-repo.sh
+++ create-repo.sh
@@ -4,8 +4,8 @@ if [[ ! -d ./personal-projects/$1 ]]; then
     mkdir ../$1
 fi
 
-git init
-git remote add origin git@github.com:$2/$1.git
+# git init
+# git remote add origin git@github.com:$2/$1.git
 
 echo "# $1" >> ../$1/README.md
 ignore_files=('venv/' '**/*.pyc' '**/*pychache' '**/*_cache' '.env')
@@ -17,6 +17,6 @@ done
 mkdir ../$1/src ../$1/tests
 touch ../$1/src/main.py ../$1/tests/test-main.py
 
-git add .
-git commit -m "initial commit"
-git push origin main
+# git add .
+# git commit -m "initial commit"
+# git push origin main
