6e46cec68a7e4cf2c88b8bca8b8d932430824094
Helen Morris <morris_helen@hotmail.com>
2023-06-08 15:16:36 +0100

    creates directories and files in correct location

--- create-repo.sh
+++ create-repo.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
 
+if [[ ! -d ./personal-projects/$1 ]]; then
+    mkdir ../$1
+fi
+
+echo "# $1" >> ../$1/README.md
+ignore_files=('venv/' '**/*.pyc' '**/*pychache' '**/*_cache' '.env')
+
+for i in "${ignore_files[@]}"; do
+    echo "$i" >> ../$1/.gitignore
+done
+
+mkdir ../$1/src ../$1/tests
+touch ../$1/src/main.py ../$1/tests/test-main.py
